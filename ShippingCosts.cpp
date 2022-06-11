466c2e27fc06fee09778588b0cfb8bcb2b28e048
IssaPortfolio <Habeeb.H.Issa@gmail.com>
2022-06-11 17:28:06 -0400

    MORE COMMENTS

--- ShippingCosts.cpp
+++ ShippingCosts.cpp
@@ -32,51 +32,49 @@
 #include <string>       // GIVES DATA TYPE "string"
 #include <algorithm>    // USED TO MANIPULATE STRINGS (UPPERCASE, LOWERCASE, ETC...)
 #include <fstream>      // OUTPUT TO A FILE
-
-
-using namespace std;
-
-const string OUTFILESTREAM = "Order.txt";
+using namespace std;    // SO NO NEED TO DO "std::cout" OR "std::cin"
 
 
 int main()
 {
-
+    // FILE FOR OUTPUT (CREATES IT IF DOES NOT EXIST)
+    const string OUTFILESTREAM = "Order.txt";
     ofstream fOutStream;
-    fOutStream.open(OUTFILESTREAM);
+    fOutStream.open(OUTFILESTREAM);  // OPENS FILE TO OUTPUT TO
 
 
     // EVERY TIME WE USE "setw(int)" THEN THE WHITESPACE WILL BE FILLED WITH '.'
+    // FIXED IS REAL NUMBER (NOT SCIENTIFIC NOTATION) AND SETPRECISION IS AMOUNT OF DECIMAL PLACES 
     cout << setfill('.') << fixed << setprecision(2);
     fOutStream << setfill('.') << fixed << setprecision(2);
 
     //// INTRODUCTION ////
-    cout << setw(51) << "\n"
-         << "ITCS 2530 - Programming Assignment for week #3\n" 
-         << setw(51) << "\n" << "\n" << endl;
+    cout        << setw(51) << "\n"
+                << "ITCS 2530 - Programming Assignment for week #3\n" 
+                << setw(51) << "\n" << "\n" << endl;
 
-    fOutStream << setw(51) << "\n"
-               << "ITCS 2530 - Programming Assignment for week #3\n"
-               << setw(51) << "\n" << "\n" << endl;
+    fOutStream  << setw(51) << "\n"
+                << "ITCS 2530 - Programming Assignment for week #3\n"
+                << setw(51) << "\n" << "\n" << endl;
 
     // CONST STRING USED FOR INVALID INPUT (ERROR MESSAGE)
     const string INVALID_INPUT_SENTENCE = "Invalid entry, exiting";
 
     ////////////////////// INPUTS /////////////////////////////
 
-    //// NAME OF ITEM ////
+    //********* NAME OF ITEM *********//
     const string ITEM_NAME_SENTENCE = "Please enter the item name (no spaces)";
     cout << right << setw(49) << left << ITEM_NAME_SENTENCE << ":";
     string item_name_input;
-    cin >> item_name_input;
+    cin >> item_name_input;  // INPUT
     fOutStream << right << setw(49) << left << ITEM_NAME_SENTENCE << ":" << item_name_input << endl;
 
 
-    //// IF FRAGILE ////
+    //********* IF FRAGILE *********//
     const string  IS_FRAGILE_SENTENCE= "Is the item fragile? (y=yes/n=no)";
     cout << right << setw(49) << left << IS_FRAGILE_SENTENCE << ":";
     string fragile_input;
-    cin >> fragile_input;
+    cin >> fragile_input;  // INPUT
     fOutStream << right << setw(49) << left << IS_FRAGILE_SENTENCE << ":" << fragile_input << endl;
     // CHECKS IF INPUT IS VALID
     if ((fragile_input != "y" && fragile_input != "Y") && (fragile_input != "n" && fragile_input != "N"))
@@ -87,19 +85,19 @@ int main()
     }
 
 
-    //// ORDER TOTAL (BEFORE SHIPPING) ////
+    //********* ORDER TOTAL (BEFORE SHIPPING) *********//
     const string  ORDER_TOTAL_SENTENCE= "Please enter your order total";
     cout << right << setw(49) << left << ORDER_TOTAL_SENTENCE << ":";
     double subtotal_input;
-    cin >> subtotal_input;
+    cin >> subtotal_input;  // INPUT
     fOutStream << right << setw(49) << left << ORDER_TOTAL_SENTENCE << ":" << subtotal_input << endl;
 
 
-    //// DESTINATION ////
+    //********* DESTINATION *********//
     const string DESTINATION_SENTENCE= "Please enter destination. (usa/can/aus)";
     cout << right << setw(49) << left << DESTINATION_SENTENCE << ":";
     string destination_input;
-    cin >> destination_input;
+    cin >> destination_input;  // INPUT
     fOutStream << right << setw(49) << left << DESTINATION_SENTENCE << ":" << destination_input << endl;
     if ((destination_input != "usa") && (destination_input != "can") && (destination_input != "aus"))
     {
@@ -114,21 +112,21 @@ int main()
 
     ////////////////////// OUTPUTS ////////////////////////////
 
-    //// NAME OF ITEM ////
+    //********* NAME OF ITEM *********//
     const string ITEM_SENTENCE = "Your item is";
     // MAKES STRING "item_name_input" ALL UPPERCASE
-    transform(item_name_input.begin(), item_name_input.end(), item_name_input.begin(), toupper);
+    transform(item_name_input.begin(), item_name_input.end(), item_name_input.begin(), toupper);  // MAKES STRING VARIABLE UPPERCASE
     cout << right << setw(40) << left << ITEM_SENTENCE << item_name_input << endl;
     fOutStream << right << setw(40) << left << ITEM_SENTENCE << item_name_input << endl;
 
 
-    //// SHIPPING COST ////
-    double shipping_cost = 0.00;
+
+    double shipping_cost = 0.00;  // DOUBLE VARIABLE (BEFORE SHIPPING COSTS ARE CALCULATED)
 
     // USA SHIPPING COST
     if (destination_input == "usa")
     {
-        if (subtotal_input >= 0 && subtotal_input < 50)
+        if (subtotal_input < 50)
             shipping_cost += 6.00;
         else if (subtotal_input >= 50 && subtotal_input <= 100)
             shipping_cost += 9.00;
@@ -141,26 +139,26 @@ int main()
     // CANADA SHIPPING COST
     else if (destination_input == "can")
     {
-        if (subtotal_input >= 0 && subtotal_input < 50)
+        if (subtotal_input < 50)
             shipping_cost += 8.00;
         else if (subtotal_input >= 50 && subtotal_input <= 100)
             shipping_cost += 12.00;
         else if (subtotal_input >= 100.01 && subtotal_input <= 150)
             shipping_cost += 15.00;
-        else
+        if (subtotal_input > 150)
             shipping_cost += 0.00;
     }
 
     // AUSTRALIA SHIPPING COST
     else if (destination_input == "aus")
     {
-        if (subtotal_input >= 0 && subtotal_input < 50)
+        if (subtotal_input < 50)
             shipping_cost += 10.00;
         else if (subtotal_input >= 50 && subtotal_input <= 100)
             shipping_cost += 14.00;
         else if (subtotal_input >= 100.01 && subtotal_input <= 150)
             shipping_cost += 17.00;
-        else
+        if (subtotal_input > 150)
             shipping_cost += 0.00;
     }
 
@@ -170,31 +168,32 @@ int main()
     else
         shipping_cost += 0;
 
+    //********* SHIPPING COST *********//
     const string SHIPPING_COST_SENTENCE = "Your shipping cost is";
     cout << right << setw(40) << left << SHIPPING_COST_SENTENCE << '$' << shipping_cost << endl;
     fOutStream << right << setw(40) << left << SHIPPING_COST_SENTENCE << '$' << shipping_cost << endl;
 
 
-    //// SHIPPING TO ////
+    //********* SHIPPING TO *********//
     const string SHIPPING_TO_SENTENCE = "You are shipping to";
-    transform(destination_input.begin(), destination_input.end(), destination_input.begin(), toupper);
+    transform(destination_input.begin(), destination_input.end(), destination_input.begin(), toupper);  // MAKES STRING VARIABLE UPPERCASE
     cout << right << setw(40) << left << SHIPPING_TO_SENTENCE << destination_input <<endl;
     fOutStream << right << setw(40) << left << SHIPPING_TO_SENTENCE << destination_input << endl;
 
 
-    //// SHIPPING TOTAL ////
+    //********* SHIPPING TOTAL *********//
     double total_cost = subtotal_input + shipping_cost;
     const string TOTAL_SHIPPING_COST_SENTENCE = "Your total shipping costs are.";
     cout << right << setw(40) << left << TOTAL_SHIPPING_COST_SENTENCE << '$' << total_cost << endl;
     fOutStream << right << setw(40) << left << TOTAL_SHIPPING_COST_SENTENCE << '$' << total_cost << endl;
 
 
-    //// THANK YOU MESSAGE ////
-    cout << "\n" << left << setfill('-') << setw(50) << endl << right << "Thank You!" << endl;
-    fOutStream << endl << endl << endl;
+    //********* THANK YOU MESSAGE *********//
+    cout << left << setfill('-') << setw(50) << endl << right << "Thank You!" << "\n" << endl;
+    fOutStream << endl << endl << endl;  // DOES NOT HAVE THANK YOU MESSAGE 
    
+    // CLOSES THE OUTPUT FILE
     fOutStream.close();
-
 
     // PAUSES COMMAND LINE RATHER THAN CLOSING
     system("pause");
@@ -206,6 +205,28 @@ int main()
 
 /*
 
+    I did not have much trouble in this code. However, I did not know how to output to a file and 
+    watched "I/O Demo 2" on Canvas.
 
+
+    I believe the way I am outputting is not ideal, such as "cout << right << setw(40) << left <<" as it is repitive 
+    but I am not sure how else I could do it. I have tried putting "setw(int)" and such where I have my defaults for
+    my output streams (line 48 and 49) but I could not get it to work properly. 
+
+
+    This is where the shipping cost is calculated, the example below is for USA.
+
+    if (destination_input == "usa")                                    We get what "destination_input" because the user inputs it. 
+    {
+        if (subtotal_input < 50)                                       A range (if subtotal_input is less than 50)
+            shipping_cost += 6.00;                                     Adds 6 to double variable "subtotal_input" if conditional is true.
+        else if (subtotal_input >= 50 && subtotal_input <= 100)        "else if" means if first conditional (line 219) was not true then it would get here but if it was true
+                                                                       then it would ignore "else if" statements (this leads to saving computer resources, or processing power).
+            shipping_cost += 9.00;
+        else if (subtotal_input >= 100.01 && subtotal_input <= 150)
+            shipping_cost += 12.00;
+        else if (subtotal_input > 150)
+            shipping_cost += 0.00;
+    }
 
 */
